391be3c830a065d16760df089a7f264d58a415d0
alvinsiew <39293717+alvinsiew@users.noreply.github.com>
2019-07-18 14:27:46 +0800

    Delete generate.sh

--- generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -vx
-
-# 64bit builder
-env GOOS=darwin GOARCH=amd64 go build -o ~/gosshbin/64bit/darwin/gossh_darwin_amd64 cmd/gossh/main.go
-shasum ~/gosshbin/64bit/darwin/gossh_darwin_amd64 | awk '{print $1}' > ~/gosshbin/64bit/darwin/gossh_darwin_amd64.shasum
-env GOOS=linux GOARCH=amd64 go build -o ~/gosshbin/64bit/linux/gossh_linux_amd64 cmd/gossh/main.go
-shasum  ~/gosshbin/64bit/linux/gossh_linux_amd64 | awk '{print $1}' >  ~/gosshbin/64bit/linux/gossh_linux_amd64.shasum
-# env GOOS=windows GOARCH=amd64 go build -o ~/gosshbin/64bit/window/gossh cmd/gossh/main.go
