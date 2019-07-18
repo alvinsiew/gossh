612c8db74d6597f4e929d34a7431dab716f4efa1
Alvin Siew <alvin_siew@tech.gov.sg>
2019-07-18 23:55:52 +0800

    Add conf test

---
+++ internal/config/config_test.go
@@ -0,0 +1,27 @@
+package config
+
+import "testing"
+import "os"
+
+func TestGetCurrentUser(t *testing.T) {
+	getuser := GetCurrentUser()
+	
+	if len(getuser.Username) <= 0 {
+		t.Errorf("Username should not be blank %v", getuser.Username )
+	}
+}
+
+func TestRandStringRunes(t *testing.T) {
+	s := RandStringRunes(10) 
+	if len(s) != 10 {
+		t.Errorf("Length of string should be 10, but got %v", len(s))
+	}
+}
+
+func TestMkDir(t *testing.T) {
+	MakeDir("testFolder")
+	if _, err := os.Stat("testFolder"); os.IsNotExist(err) {
+		t.Errorf("Cannot mkdir testFolder")
+	}
+	os.Remove("testFolder")
+}
